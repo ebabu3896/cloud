35dd8fd694cf8a9ea20fab9c61d1d80d26232d19
ebabu3896 <sarathbabue3896@gmail.com>
2024-02-08 17:15:19 +0530

    implementation of trie

--- Advanced Algorithms/trie prefixsum implementation/implement trie.py
+++ Advanced Algorithms/trie prefixsum implementation/implement trie.py
@@ -1,1 +1,41 @@
+# Implementation of trie
 
+class TrieNode:
+    def __init__(self):
+        self.childern = {}
+        self.endOfWord = False
+
+class Trie:
+    def __init__(self):
+        '''
+        Intalize the data strucutre
+        '''
+        self.root = TrieNode()
+
+    def insert(self, word):
+
+        cur = self.root
+
+        for c in word:
+            if c not in cur.childern:
+                cur.childern[c] = TrieNode()
+            cur = cur.childern[c]
+        cur.endOfWord = True
+    
+    def search(self, word):
+        cur = self.root
+        for c in word:
+            if c not in cur.childern:
+                return False
+            cur = cur.childern[c]
+    
+        return cur.endOfWord
+    
+    def startsWith(self, prefix):
+        cur = self.root
+
+        for c in prefix:
+            if c not in cur.childern:
+                return False
+            cur = cur.childern[c]
+        return True
