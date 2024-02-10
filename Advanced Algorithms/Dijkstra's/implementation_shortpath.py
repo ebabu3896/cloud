916a0fadde0481dc8d92eb4721d7d50b5b616b94
ebabu3896 <sarathbabue3896@gmail.com>
2024-02-10 06:10:01 +0530

    Adding new Aglo and test cases

---
+++ Advanced Algorithms/Dijkstra's/implementation_shortpath.py
@@ -0,0 +1,25 @@
+# Given a connected graph represented by a list of edges, where edge[0] = src, edge[1] = dst, edge[2] = weight.
+# Find the shortest path from src to every other node in the graph. there are n nodes in that graph
+# O(E * logV), O(E * logE) is also correct.
+import heapq
+def shortestPath(edges, n, src):
+    adj = {}
+    for i in range(1, n + 1):
+        adj[i] = []
+    
+    # s = src, d = dst, w = weight
+    for s, d, w in edges:
+        adj[s].append((d, w))
+    
+    shortest = {}
+    minHeap = [(0, src)]
+    while minHeap:
+        w1, n1 = heapq.heappop(minHeap)
+        if n1 in shortest:
+            continue
+        shortest[n1] = w1
+
+        for n2, w2 in adj[n1]:
+            if n2 not in shortest:
+                heapq.heappush(minHeap, (w1 + w2, n2))
+    return shortest
